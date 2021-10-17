017c85d9071ecf40390153a1e6921a142fcb7b78
Kevin Lidstone <kevin.lidstone@solace.com>
2021-10-17 13:52:17 -0400

    Fix return to board

--- src/app.js
+++ src/app.js
@@ -476,6 +476,7 @@ export class App {
   }
 
   returnToBoard() {
+    this.questionVisible = false;
     let done = this.categories.filter(category => {
       let r = category.answers.filter(answer => {
         return answer.viewed;
